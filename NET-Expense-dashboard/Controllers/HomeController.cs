4869ae137888f3dae5dad506ba5aa76492195ab7
K2adir <k2adir@gmail.com>
2023-05-13 22:03:39 -0400

    initial

---
+++ NET-Expense-dashboard/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NET_Expense_dashboard.Models;
+
+namespace NET_Expense_dashboard.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ NET-Expense-dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NET_Expense_dashboard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
